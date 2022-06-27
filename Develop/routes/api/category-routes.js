75bd4a7117ec2c0e02f4a5dd85d5d624d454cf0d
Ben Grinthal <bgrinthal@gmail.com>
2022-06-27 09:30:42 -0400

    fixed bug with category post route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -39,10 +39,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const locationData = await Category.create({
-      category_id: req.body.reader_id,
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
     });
-    res.status(200).json(locationData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
